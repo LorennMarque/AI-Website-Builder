3ab4c37e64d54a15ffdd592e59c2221e00016f52
lorenzoMarquesini <lorenzomarquesini@gmail.com>
2024-07-24 12:57:59 -0300

    Login

--- server/login.php
+++ server/login.php
@@ -10,16 +10,23 @@ $sql = "SELECT id, password, active FROM users WHERE email = '$email' AND active
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
-    // Usuario encontrado
+    // Usuario encontrado|
     $row = $result->fetch_assoc();
     $hashedPassword = $row['password'];
     
     // Verificar la contraseña
     if (password_verify($password, $hashedPassword)) {
-        // Iniciar sesión
+        // Iniciar sesión y obtener el id del sitio web asociado al usuario
         session_start();
         $_SESSION['user_id'] = $row['id'];
-        echo "Login successful";
+        $websiteIdSql = "SELECT id FROM website_information WHERE user_id = " . $row['id'];
+        $websiteResult = $conn->query($websiteIdSql);
+        if ($websiteResult->num_rows > 0) {
+            $websiteRow = $websiteResult->fetch_assoc();
+            echo json_encode(array("success" => true, "website_id" => $websiteRow['id']));
+        } else {
+            echo json_encode(array("success" => false, "message" => "Website ID not found"));
+        }
     } else {
         echo "Invalid email or password.";
     }
